c1900a651442fee016d997e9bb18d953508b3319
Eliort Kid <eliotfonse99@gmail.com>
2025-03-13 14:54:35 -0300

    feat: add schemas and custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/EliotyKid/first-api-go.git/router"
+import (
+	"github.com/EliotyKid/first-api-go.git/config"
+	"github.com/EliotyKid/first-api-go.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Routes
 	router.Initialize()
 }
